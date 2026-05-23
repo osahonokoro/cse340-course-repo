9ba3f97438a8711ba3fc77e4e288680fa74dd5af
osahonokoro <osahonokoro@gmail.com>
2026-05-23 09:27:48 +0100

    Fix db.js to use environment variable for database connection

--- src/models/db.js
+++ src/models/db.js
@@ -1,15 +1,13 @@
 ﻿import pg from "pg";
 const { Pool } = pg;
 
-console.log("Connecting to local PostgreSQL...");
+console.log("Connecting to PostgreSQL...");
 
 const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    database: "cse340_db",
-    user: "postgres",
-    password: "Gal2v20@78",
-    ssl: false,
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
     connectionTimeoutMillis: 10000,
 });
 
